8007a95523a60de0714833780c8e42363d071217
wararyo <wararyo@gmail.com>
2019-04-23 01:43:35 +0900

    Snap.svgでロゴアニメーション

--- src/App.vue
+++ src/App.vue
@@ -25,4 +25,12 @@ export default {
 svg {
   fill: currentColor;
 }
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 </style>
--- src/layout/Hero.vue
+++ src/layout/Hero.vue
@@ -1,10 +1,15 @@
 <template>
     <header>
+        <div class="hero-background">
+
+        </div>
         <div class="hero-heading">
             <navigation />
             <share />
         </div>
-        <h1><img src="" alt="CEiQ"></h1>
+        <h1 id="hero-svg-logo">
+            
+        </h1>
         <div class="hero-description">
             <p><span class="is-big">2019</span>年 <span class="is-big">9/22</span>(日) 13:00 -</p>
             <p><span class="is-big">九州大学 大橋キャンパス</span>にて開催</p>
@@ -18,13 +23,52 @@
 
 <script>
 import Navigation from '@/components/Navigation.vue';
-import Share from '@/components/Share.vue'
+import Share from '@/components/Share.vue';
+
 export default {
     components: {
         Navigation,
         Share
     }
 }
+
+//ロゴのSVGアニメーション
+var jsonfile = "SnapSVGAnimator/logo.json",
+    fps = 60,
+    width = 480,
+    height = 640,
+    AJAX_req;
+
+AJAX_JSON_Req(jsonfile);
+
+function handle_AJAX_Complete() {
+    if( AJAX_req.readyState == 4 && AJAX_req.status == 200 )
+    {
+        let json = JSON.parse(AJAX_req.responseText);
+        let comp = new SVGAnim(
+                        json,
+                        width,
+                        height,
+                        fps
+                        );
+        // SVG 要素を所定の位置に移動させる
+        var el = document.querySelector("svg[id^=svg]");
+        var container = document.querySelector("#hero-svg-logo");
+        container.appendChild(el);
+        
+    }
+}
+
+function AJAX_JSON_Req( url )
+{
+    AJAX_req = new XMLHttpRequest();
+    AJAX_req.open("GET", url, true);
+    AJAX_req.setRequestHeader("Content-type", "application/json");
+    
+    AJAX_req.onreadystatechange = handle_AJAX_Complete;
+    AJAX_req.send();
+}
+
 </script>
 
 <style lang="scss" scoped>
@@ -34,14 +78,26 @@ header {
     display: flex;
     flex-direction: column;
     align-items: center;
-    background: linear-gradient(122deg,#f5f0b1 0%, #f5bfc6 50%, #c5eeef 100%);
+    overflow: hidden;
     box-shadow: 0px 3px 6px rgba(112, 40, 52, 0.2);
+    .hero-background {
+        z-index: -1;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background: linear-gradient(122deg,#f5f0b1 0%, #f5bfc6 50%, #c5eeef 100%);
+        background-attachment: fixed;
+        animation: fade-in 1s ease 2s 1 normal both running;
+    }
     .hero-heading {
         align-self: stretch;
         display: flex;
         justify-content: space-between;
         align-items: center;
         padding: 36px;
+        animation: fade-in 1s ease 1.8s 1 normal both running;
     }
     h1 {
         flex: 1;
@@ -54,6 +110,7 @@ header {
         text-align: center;
         font-weight: bold;
         color: rgba(#000,.75);
+        animation: fade-in 1s ease 2.2s 1 normal both running;
         .is-big {
             font-size: 1.5em;
         }
@@ -83,3 +140,18 @@ header {
 }
 </style>
 
+<style lang="scss">
+header h1 svg {
+    animation: logo-pop 0.5s ease;
+}
+@keyframes logo-pop {
+    0% {
+        transform: scale(2);
+        opacity: 0;
+    }
+    100% {
+        transform: scale(1);
+        opacity: 1;
+    }
+}
+</style>
