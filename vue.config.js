07e22adb697bbb501b5f52c0901a62568c530026
wararyo <wararyo@gmail.com>
2019-04-23 16:17:27 +0900

    npm版snapsvgに関する記述を削除

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-    chainWebpack: config => {
-        config.module
-            .rule("snapsvg")
-            .test(require.resolve("snapsvg"))
-            .use("imports-loader?this=>window,fix=>module.exports=0")
-            .loader("imports-loader")
-            .end();
-    }
+
 }
