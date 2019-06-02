19e575e295b9266bb5bfd570782b14cec2b263fe
wararyo <wararyo@gmail.com>
2019-06-03 03:11:26 +0900

    SectionHeaderのアニメーション

--- src/components/SectionHeader.vue
+++ src/components/SectionHeader.vue
@@ -1,6 +1,6 @@
 <template>
-    <h2 class="sectionheader" :class="{'is-small':isSmall}">
-        <span class="sectionheader-main">{{title}}</span>
+    <h2 class="sectionheader" :class="{'is-small':isSmall, 'hidden':!isVisible}">
+        <span class="sectionheader-main"><span v-for="item in letters" :key="item.id" :style="item.delay" v-html="item.text"></span></span>
         <span class="sectionheader-sub">{{subtitle}}</span>
     </h2>
 </template>
@@ -8,6 +8,48 @@
 <script>
 export default {
     props: ['title', 'subtitle', 'isSmall'],
+    data: function() {
+        return {
+            height: 0,
+            position: 0,
+            isVisible: false
+        };
+    },
+    computed: {
+        letters: function() {
+            const l = [];
+            const _texts = this.title.split('');
+            for(let i = 0;i < _texts.length; i++) {
+                l.push({
+                    text: (_texts[i] === " ")?`&nbsp;`:_texts[i],
+                    id: i,
+                    delay: `animation-delay: ${i*0.02}s`
+                });
+            }
+            return l;
+        }
+    },
+    watch: {
+        position: function() {
+            if(this.position < this.height*0.9) this.isVisible = true;
+        }
+    },
+    mounted() {
+        window.addEventListener('resize', this.onResize);
+        window.addEventListener('scroll', this.onScroll);
+        window.addEventListener('load', ()=> {
+            this.onResize();//heightを先に代入する必要がある
+            this.onScroll();
+        });
+    },
+    beforeDestroy() {
+        window.removeEventListener('resize', this.onResize);
+        window.removeEventListener('scroll', this.onScroll);
+    },
+    methods: {
+        onScroll () {this.position = (this.$el)?this.$el.getBoundingClientRect().top:this.height;},
+        onResize () {this.height = document.documentElement.clientHeight}
+    }
 }
 </script>
 
@@ -15,7 +57,7 @@ export default {
 .sectionheader {
     display: inline-block;
     margin: 100px;
-    span {
+    > span {
         display: block;
         font-weight: normal;
         white-space: nowrap;
@@ -29,6 +71,10 @@ export default {
         font-style: italic;
         font-weight: 700;
         color: #444;
+        overflow: hidden;
+        > span {
+            display: inline-block;
+        }
     }
     .sectionheader-sub {
         font-size: 1rem;
@@ -37,10 +83,34 @@ export default {
         color: #FFF;
         background-color: #444;
         border-radius: 16px;
+        transition: color .7s cubic-bezier(0.04, 0.79, 0.3, 1);
+    }
+    &.hidden {
+        .sectionheader-main {
+            visibility: hidden;
+        }
+        .sectionheader-sub {
+            color: rgba(white,.0);
+        }
+    }
+    &:not(.hidden) {
+        .sectionheader-main > span {
+            animation: slide-in 0.7s cubic-bezier(0.04, 0.79, 0.3, 1) 1 normal both running;
+        }
     }
     &.is-small {
         .sectionheader-main {
             font-size: 3rem;
+        }
+    }
+    @keyframes slide-in {
+        0% {
+            transform:translateY(100%);
+            opacity: 0;
+        }
+        100% {
+            transform:translateY(0);
+            opacity: 1;
         }
     }
 }
