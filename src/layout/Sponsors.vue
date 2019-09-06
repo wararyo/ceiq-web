4cf59615d68eb7ca978db6b7df2e47d2c1efa643
wararyo <wararyo@gmail.com>
2019-09-06 17:25:36 +0900

    調整

--- src/layout/Sponsors.vue
+++ src/layout/Sponsors.vue
@@ -5,7 +5,7 @@
             <img class="" src="@/assets/kooki.svg" width="297" alt="空気株式会社">
             <img class="" src="@/assets/wacom.svg" width="297" alt="株式会社ワコム">
         </div>
-        <!-- <div class="comingsoon">And so on...</!--> -->
+        <!-- <div class="comingsoon">And so on...</div> -->
     </section>
 </template>
 
